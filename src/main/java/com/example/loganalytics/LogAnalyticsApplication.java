39b230ae0356463a2e622732a824bb34b4e8f2dc
Hoang Phong <hoangvanphong579@gmail.com>
2025-11-30 15:14:21 +0700

    first commit

---
+++ src/main/java/com/example/loganalytics/LogAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.loganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loganalytics/LogAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loganalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
